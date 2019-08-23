f203aa3658c1336aa3eac645a1be48a2bb9db62e
mkdir-not-war <gavin_gr@msn.com>
2019-08-23 11:08:04 -0500

    starting local map

---
+++ worldgen/localmap.py
@@ -0,0 +1,13 @@
+class LocalTile:
+	def __init__(self):
+		pass
+
+class LocalMap:
+	def __init__(self, x, y, region):
+		regiontile = region.regiontile(x, y)
+
+def main():
+	pass
+
+if __name__=='__main__':
+	main()
